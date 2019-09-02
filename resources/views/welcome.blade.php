8b49d0148d8a113c0516dad1e86fa3bac1e44cd4
Simon Dann <simon.dann@gmail.com>
2019-09-02 23:05:23 +0100

    stubs in some welcome page content for layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,17 @@
 @extends('layouts.public')
 
+@section('content')
+    <section class="border-bottom py-5">
+        <div class="container">
+            <div class="row ">
+                <div class="col">
+                    <h1>Welcome to FediCast</h1>
+                </div>
+                <div class="col">
+                    2 of 2
+                </div>
+            </div>
 
+        </div>
+    </section>
+@endsection
