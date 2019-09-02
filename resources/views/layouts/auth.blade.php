b8eb16de8f6204ec6d8437e1b030c9dbe2706f32
Simon Dann <simon.dann@gmail.com>
2019-09-02 21:00:42 +0100

    adds a large margin to the footer in auth views

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -13,7 +13,7 @@
         @yield('content')
     </main>
 
-    <footer class="container text-center">
+    <footer class="container text-center my-5">
         Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
                 href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
     </footer>
