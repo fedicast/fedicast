0a93a4fd67c61641088e2ff5364b36b8dcea7e9f
Simon Dann <simon.dann@gmail.com>
2019-09-02 23:05:07 +0100

    stubs in some sub navigation test content

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts.public')
 
+@section('sub-navigation')
+    <strong class="navbar-brand">Pricing</strong>
+    <div class="collapse navbar-collapse small" id="subNavigation">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">{{ __('Plans') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">{{ __('Support') }}</a>
+            </li>
+        </ul>
+    </div>
+@endsection
 
+@section('content')
+    <h1>Pricing</h1>
+@endsection
