ac67d0df628c9c07a4b97b5fbf174bca3238aa3f
Simon Dann <simon.dann@gmail.com>
2019-09-03 21:41:06 +0100

    adds mic.svg and attribution

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -89,5 +89,8 @@
             Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
                 href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
         </p>
+        <p>
+            <a href="https://thenounproject.com/browse/?i=2817022#">Mic svg by Oleksandr Panasovskyi from the Noun Project</a>
+        </p>
     </footer>
 @endsection
