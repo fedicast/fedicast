472e27f372bc30d81eb18e740870cf244f14f661
Simon Dann <simon.dann@gmail.com>
2019-09-03 20:58:09 +0100

    adds a layout for the blog search bar

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.public')
 
-@section('sub-navigation')
-    <strong class="navbar-brand">Blog</strong>
-    <div class="collapse navbar-collapse small" id="subNavigation">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#">{{ __('Sublink One') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">{{ __('Sublink Two') }}</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">{{ __('Sublink Three') }}</a>
-            </li>
-        </ul>
-    </div>
+@section('banner-header')
+    <section class="bg-white border-bottom pt-5 pb-6">
+        <div class="container">
+            <h1 class="mb-5">Blog</h1>
+
+
+            <form class="form-inline">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text border-0 bg-transparent px-0" id="search-add-on">
+                            <i class="fas fa-search"></i>
+                        </span>
+                    </div>
+                    <input name="s" type="text" class="form-control border-0" placeholder="Search our blog..." aria-label="Search" aria-describedby="search-add-on">
+                </div>
+            </form>
+
+        </div>
+    </section>
 @endsection
 
 @section('content')
-    <h1>Blog</h1>
+
 @endsection
