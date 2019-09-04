c16a68b18c7eeeacef4bacd561f18dcd061a2a4f
Simon Dann <simon.dann@gmail.com>
2019-09-04 22:52:16 +0100

    adds references

--- resources/views/references.blade.php
+++ resources/views/references.blade.php
@@ -16,14 +16,35 @@
 
 @section('content')
     <div class="container py-4">
-        <div>
-            <p>
-                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
-                    href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-            </p>
-            <p>
-                <a href="https://thenounproject.com/browse/?i=2817022#">Mic svg by Oleksandr Panasovskyi from the Noun Project</a>
-            </p>
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="min-height: 225px;">
+                    <div class="card-body">
+                        <h5 class="card-title">FediCast Logo</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Wifi free icon by Freepik</h6>
+                        <p class="card-text">
+                            The icon used as the "logo" for the FediCast project came from the coffee shop
+                            pack by freepik on Flaticon.
+                        </p>
+                        <a href="https://www.flaticon.com/free-icon/wifi_808858" class="card-link">Source</a>
+                        <a href="https://www.flaticon.com/authors/freepik" class="card-link">Author</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card" style="min-height: 225px;">
+                    <div class="card-body">
+                        <h5 class="card-title">Welcome page Mic Vector</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Mic svg by Oleksandr Panasovskyi</h6>
+                        <p class="card-text">
+                            The vector used on the welcome page was sourced from the Noun Project.
+                        </p>
+                        <a href="https://thenounproject.com/browse/?i=2817022#" class="card-link">Source</a>
+                        <a href="https://thenounproject.com/a.panasovsky/" class="card-link">Author</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
