d68b73546c33f7d97c5cf797ce89d9dd509273f6
Simon Dann <simon.dann@gmail.com>
2019-09-05 20:18:22 +0100

    adds missing platform pages

---
+++ resources/views/platform/features.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.platform', ['title' => __('Features')])
+
+@section('content')
+    <div class="container py-4">
+        <div>Main content...</div>
+    </div>
+@endsection
---
+++ resources/views/platform/federation.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.platform', ['title' => __('Federation')])
+
+@section('content')
+    <div class="container py-4">
+        <div>Main content...</div>
+    </div>
+@endsection
