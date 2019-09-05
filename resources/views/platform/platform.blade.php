61972fd79f1aba40d51cbe90da6ebb2d74959afd
Simon Dann <simon.dann@gmail.com>
2019-09-05 22:07:08 +0100

    adds a little bit of thought on how the platform page might look

--- resources/views/platform/platform.blade.php
+++ resources/views/platform/platform.blade.php
@@ -1,7 +1,15 @@
-@extends('layouts.platform', ['title' => __('Platform')])
+@extends('layouts.platform', ['title' => __('Platform'), 'darkBg' => true])
 
 @section('content')
-    <div class="container py-4">
-        <div>Main content...</div>
+    <div class="container py-4" style="    min-height: 800px;">
+        <div class="text-left py-4">
+            <h1 class="w-75">
+                The only podcast publishing platform to federate with Mastodon
+            </h1>
+            <h2 class="h4 mt-4 w-50">
+                Putting the ActivityPub into Public Radio, FediCasts gives you everything you need to publish on both
+                traditional and federated mediums.
+            </h2>
+        </div>
     </div>
 @endsection
