f10dc2cc8074f1080e226d1144621a42565788e7
Simon Dann <simon.dann@gmail.com>
2019-09-07 00:16:48 +0100

    adds bottom border to current links

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
-
 @section('sub-navigation')
     <div class="collapse navbar-collapse" id="subNavigation">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav- mr-2" style="border-bottom: 3px solid #d8d8d8;">
+        <ul class="navbar-nav with-bottom-bordered-current mr-auto">
+            <li class="nav-item {{ (request()->routeIs('user.overview')) ? 'current-nav-item' : '' }} mr-2">
                 <a class="nav-link" href="{{ route('user.overview', ['username' => \Auth::user()->name]) }}">{{ __('Overview') }}</a>
             </li>
-            <li class="nav-item mr-2">
+            <li class="nav-item {{ (request()->routeIs('user.podcasts')) ? 'current-nav-item' : '' }} mr-2">
                 <a class="nav-link" href="{{ route('user.podcasts', ['username' => \Auth::user()->name]) }}">{{ __('Podcasts') }}</a>
             </li>
-            <li class="nav-item mr-2">
+            <li class="nav-item {{ (request()->routeIs('user.domains')) ? 'current-nav-item' : '' }} mr-2">
                 <a class="nav-link" href="{{ route('user.domains', ['username' => \Auth::user()->name]) }}">{{ __('Domains') }}</a>
             </li>
-            <li class="nav-item mr-2">
+            <li class="nav-item {{ (request()->routeIs('user.usage')) ? 'current-nav-item' : '' }} mr-2">
                 <a class="nav-link" href="{{ route('user.usage', ['username' => \Auth::user()->name]) }}">{{ __('Usage') }}</a>
             </li>
-            <li class="nav-item mr-2">
+            <li class="nav-item {{ (request()->routeIs('user.settings')) ? 'current-nav-item' : '' }} mr-2">
                 <a class="nav-link" href="{{ route('user.settings', ['username' => \Auth::user()->name]) }}">{{ __('Settings') }}</a>
             </li>
         </ul>
