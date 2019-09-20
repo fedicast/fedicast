3e9ac899aac8627973b2aa13c4acc946c48d97ef
Simon Dann <simon.dann@gmail.com>
2019-09-20 22:59:01 +0100

    makes persist method require a message bag param so messages can be passed

--- app/Http/Requests/AbstractBaseRequest.php
+++ app/Http/Requests/AbstractBaseRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\MessageBag;
 
 abstract class AbstractBaseRequest extends FormRequest
 {
@@ -17,9 +18,10 @@ abstract class AbstractBaseRequest extends FormRequest
     /**
      * This method persists the model with any changes required for this request.
      *
+     * @param MessageBag $messageBag
      * @return bool
      */
-    public function persist(): bool
+    public function persist(MessageBag $messageBag): bool
     {
         return false;
     }
