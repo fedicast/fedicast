94d24667d9256914e713a0a0c43750b2cd7248ee
Simon Dann <simon.dann@gmail.com>
2019-09-21 22:42:41 +0100

    adds some incomplete test stubs

---
+++ tests/Feature/UserAccountTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserAccountTest extends TestCase
+{
+    public function test_a_user_can_update_their_username()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_a_user_can_update_their_display_name()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_a_user_can_update_their_email_address()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_a_user_can_update_their_avatar()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_a_user_can_delete_their_account()
+    {
+        $this->markTestIncomplete();
+    }
+}
