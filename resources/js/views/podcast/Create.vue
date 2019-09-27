071732bad4a1da8c8b9b4965b01544b4733ff9fb
Simon Dann <simon.dann@gmail.com>
2019-09-27 22:52:48 +0100

    adds beginning of create podcast form

---
+++ resources/js/views/podcast/Create.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container py-6">
+        <div class="row justify-content-center">
+            <div class="col-7">
+                <username-form persist-btn-text="messages.next">
+                    Each podcast has its own Fediverse account used for publishing to and interacting with the Fediverse.
+                    This will be the <em>username</em> that interaction happens via.
+                </username-form>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UsernameForm from "../../components/forms/Username";
+    export default {
+        name: 'create-podcast-form',
+        components: {UsernameForm}
+    }
+</script>
