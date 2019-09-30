511ac74573c585879626602c176f8c2f7a401c7e
Simon Dann <simon.dann@gmail.com>
2019-09-30 23:26:23 +0100

    passes min-length as number not string

--- resources/js/views/podcast/Create.vue
+++ resources/js/views/podcast/Create.vue
@@ -2,7 +2,7 @@
     <div class="container py-6">
         <div class="row justify-content-center">
             <div class="col-7">
-                <username-form persist-btn-text="messages.next" min-length="3">
+                <username-form persist-btn-text="messages.next" :min-length="3">
                     Each podcast has its own Fediverse account used for publishing to and interacting with the Fediverse.
                     This will be the <em>username</em> that interaction happens via.
                 </username-form>
