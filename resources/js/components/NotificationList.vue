a970d095dd59ce5ceaba40930d15222b23e141a5
Simon Dann <simon.dann@achiive.co.uk>
2019-10-03 22:44:33 +0100

    adds some filter functionality to the notifications view

---
+++ resources/js/components/NotificationList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-if="hasMessages" class="card mb-4">
+        <div class="card-header py-2 d-flex align-items-center">
+            <strong class="flex-grow-1 m-0">{{ podcast.name }}</strong>
+            <a href="#" class="btn btn-xs btn-outline-dark">✔</a>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li v-bind:key="message.id" v-for="message in messages" class="list-group-item d-flex align-items-center">
+                <span class="flex-grow-1">{{ message.title }}</span> <a href="#" class="btn btn-xs btn-outline-dark">✔</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'notification-list',
+      props: {
+        podcast: {
+          type: Object,
+          required: true,
+          default () {
+            return {name: 'Untiled'};
+          },
+        },
+        messages: {
+          type: Array,
+          default () {
+            return [];
+          }
+        }
+      },
+      data () {return{
+        title: 'Podcast Name',
+      }},
+      computed: {
+        hasMessages () {
+          return this.messages.length > 0;
+        }
+      },
+    }
+</script>
--- resources/js/views/Notifications.vue
+++ resources/js/views/Notifications.vue
@@ -2,53 +2,211 @@
     <div class="container py-6">
         <div class="row">
             <div class="col-3">
-                <a href="#" class="btn btn-md btn-info text-light w-100 text-left mb-1 d-flex align-items-center">
-                    <span class="flex-grow-1">{{'messages.unread' | trans | ucfirst }}</span>
-                    <span class="badge badge-light">16</span>
+                <router-link :class="filterClass('category:' + filter.id, ['btn-info text-light'], ['btn-light'])"
+                             :to="{ name: 'notifications', query: {'filter': 'category:' + filter.id}}"
+                             v-bind:key="filter.id" v-for="filter in categories"
+                             class="btn btn-md w-100 text-left mb-1 d-flex align-items-center">
+                    <span class="flex-grow-1">{{ filter.name | trans | ucfirst }}</span>
+                    <span class="badge badge-light">{{ filter.count }}</span>
                     <span class="sr-only">unread messages</span>
-                </a>
-                <a href="#" class="btn btn-md btn-light w-100 text-left mb-1 d-flex align-items-center">
-                    <span class="flex-grow-1">{{'messages.read' | trans | ucfirst}}</span>
-                    <span class="badge">7</span>
-                    <span class="sr-only">unread messages</span>
-                </a>
-                <a href="#" class="btn btn-md btn-light w-100 text-left mb-1 d-flex align-items-center">
-                    <span class="flex-grow-1">{{'messages.bookmarked' | trans | ucfirst }}</span>
-                    <span class="badge">3</span>
-                    <span class="sr-only">unread messages</span>
-                </a>
-                <a href="#" class="btn btn-md btn-light w-100 text-left mb-1 d-flex align-items-center">
+                </router-link>
+
+                <router-link :class="filterClass('all', ['font-weight-bold'], ['text-muted'])"
+                             :to="{name: 'notifications', query: {filter: 'all'}}"
+                             class="btn btn-md btn-light w-100 text-left mb-1 d-flex align-items-center">
                     <small>{{'messages.all notifications' | trans | ucfirst }}</small>
-                </a>
+                </router-link>
 
                 <hr/>
 
-                <a href="#" class="btn btn-sm btn-light w-100 text-left mb-1 d-flex align-items-center">
-                    <span class="flex-grow-1">Podcast Name</span>
-                    <span class="badge">3</span>
+                <router-link :class="filterClass('podcast:' + filter.id, ['font-weight-bold'], ['text-muted'])"
+                             :to="{ name: 'notifications', query: {'filter': 'podcast:' + filter.id}}"
+                             v-bind:key="filter.id" v-for="filter in podcasts"
+                             class="btn btn-sm btn-light w-100 text-left mb-1 d-flex align-items-center">
+                    <span class="flex-grow-1">{{ filter.name }}</span>
+                    <span class="badge">{{ filter.count }}</span>
                     <span class="sr-only">unread messages</span>
-                </a>
-
-                <a href="#" class="btn btn-sm btn-light w-100 text-left mb-1 d-flex align-items-center">
-                    <span class="flex-grow-1">Team Name / Podcast Name</span>
-                    <span class="badge">3</span>
-                    <span class="sr-only">unread messages</span>
-                </a>
+                </router-link>
             </div>
 
             <div class="col">
-                <div class="card mb-4">
-                    <div class="card-header py-2 d-flex align-items-center">
-                        <strong class="flex-grow-1 m-0">Podcast Name</strong>
-                        <a href="#" class="btn btn-xs btn-outline-dark">✔</a>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex align-items-center"><span class="flex-grow-1">Cras justo odio</span> <a href="#" class="btn btn-xs btn-outline-dark">✔</a></li>
-                        <li class="list-group-item d-flex align-items-center"><span class="flex-grow-1">Dapibus ac facilisis in</span> <a href="#" class="btn btn-xs btn-outline-dark">✔</a></li>
-                        <li class="list-group-item d-flex align-items-center"><span class="flex-grow-1">Vestibulum at eros</span> <a href="#" class="btn btn-xs btn-outline-dark">✔</a></li>
-                    </ul>
-                </div>
+                <template v-if="hasMessagesForFilter">
+                <notification-list v-bind:key="podcast + '_list'"
+                                   :messages="messages"
+                                   :podcast="getPodcastById(podcast)"
+                                   v-for="(messages, podcast) in filteredGroupedByPodcast"/>
+                </template>
+                <p v-else>No Messages</p>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+  import NotificationList from "../components/NotificationList";
+
+  export default {
+    name: 'Notifications',
+    components: {NotificationList},
+    mounted() {
+      // inject dummy data for local test
+
+      const that = this;
+
+      const addMessage = function (iiii) {
+        const max = 3 + Math.round(Math.random() * 8);
+        const uuid = 'xxxx-xxxx-xxxx-' + iiii;
+
+        for (let i = 0; i < max; i++) {
+          const id = 'xxxx-xxxx-' + (1000 + i) + '-' + iiii;
+          that.messages.push({
+            id: id,
+            podcast: uuid,
+            title: 'Cras justo odio',
+            meta: {
+              read: false,
+              bookmarked: false,
+            }
+          })
+        }
+      };
+
+      addMessage(1111);
+      addMessage(2222);
+    },
+    data() {
+      return {
+        filters: [
+          {
+            id: 'unread',
+            type: 'category',
+            name: 'messages.unread',
+            count: Math.round(3 + Math.random() * 30),
+          },
+          {
+            id: 'read',
+            type: 'category',
+            name: 'messages.read',
+            count: Math.round(3 + Math.random() * 30),
+          },
+          {
+            id: 'bookmarked',
+            type: 'category',
+            name: 'messages.bookmarked',
+            count: Math.round(3 + Math.random() * 30),
+          },
+
+          {
+            id: 'xxxx-xxxx-xxxx-1111',
+            type: 'podcast',
+            name: 'My Podcast',
+            count: Math.round(3 + Math.random() * 30),
+          },
+          {
+            id: 'xxxx-xxxx-xxxx-2222',
+            type: 'podcast',
+            name: 'Team Name / Podcast Name',
+            count: Math.round(3 + Math.random() * 30),
+          }
+        ],
+        messages: []
+      }
+    },
+    computed: {
+      total() {
+        return this.messages.length;
+      },
+
+      currentFilter() {
+        return this.$route.query.filter || 'category:unread';
+      },
+
+      currentFilterProps() {
+        let props = {
+          all: false,
+          category: null,
+          podcast: null,
+        };
+        const segments = this.currentFilter.split(',');
+        segments.forEach((el) => {
+          const [key, value] = el.split(':');
+          props[key] = typeof value === 'undefined' ? true : value;
+        });
+        return props;
+      },
+
+      categories() {
+        return this.filters.filter((el) => el.type === 'category');
+      },
+
+      podcasts() {
+        return this.filters.filter((el) => el.type === 'podcast');
+      },
+
+      filteredGroupedByPodcast() {
+        let buckets = {};
+        this.filteredMessages.forEach((el) => {
+          if (!buckets[el.podcast]) {
+            buckets[el.podcast] = [];
+          }
+          buckets[el.podcast].push(el);
+        });
+
+        return buckets;
+      },
+
+      filteredMessages() {
+        if (this.currentFilterProps.all === true) {
+          return this.messages;
+        }
+
+        let filtered = [];
+        let exists = [];
+
+        const addToFiltered = function (el) {
+          if (exists.includes(el.id)){ return; }
+          filtered.push(el);
+          exists.push(el.id);
+        };
+
+        if (this.currentFilterProps.category !== null) {
+          const cat = this.currentFilterProps.category;
+          if (cat === 'unread') {
+            this.messages.filter((el) => {
+              return el.meta.read === false;
+            }).forEach(addToFiltered);
+          } else if (cat === 'read') {
+            this.messages.filter((el) => {
+              return el.meta.read === true;
+            }).forEach(addToFiltered);
+          } else if (cat === 'bookmarked') {
+            this.messages.filter((el) => {
+              return el.meta.bookmarked === true;
+            }).forEach(addToFiltered);
+          }
+        }
+
+        if (this.currentFilterProps.podcast !== null) {
+          const podcast = this.currentFilterProps.podcast;
+          this.messages.filter((el) => {
+            return el.podcast === podcast;
+          }).forEach(addToFiltered);
+        }
+
+        return filtered;
+      },
+
+      hasMessagesForFilter() {
+        return this.filteredMessages.length > 0;
+      }
+    },
+    methods: {
+      getPodcastById (id) {
+        return this.podcasts.find((el => el.id === id));
+      },
+      filterClass(filter = '', is = [], not = []) {
+        return (this.currentFilter === filter) ? is : not
+      },
+    }
+  }
+</script>
