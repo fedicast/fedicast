cf42f6944a3aaaf87b0228c717d21ddfdaccca25
Simon Dann <simon.dann@gmail.com>
2019-10-24 22:01:45 +0100

    adds activity log on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,10 +63,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        activity()
+            ->causedBy($user)
+            ->log("You created your account.");
+
+        return $user;
     }
 }
