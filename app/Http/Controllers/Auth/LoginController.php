8df55788c4980df231cc425b0f9043f4138f4f54
Simon Dann <simon.dann@gmail.com>
2019-10-24 21:57:55 +0100

    adds activity log on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param mixed $user
+     * @return void
+     */
+    protected function authenticated(Request $request, User $user)
+    {
+        $userAgent = $request->header('User-Agent');
+        $browser = new \WhichBrowser\Parser($userAgent);
+
+        activity()
+            ->causedBy($user)
+            ->withProperties(['user-agent' => $userAgent])
+            ->log("Logged in from the {$browser->browser->name} browser on {$browser->os->toString()}");
     }
 }
